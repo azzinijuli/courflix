5a5272af7475e6a068d29b6de7812b3f630f3e58
Julieta Azzini <juliazzini1@gmail.com>
2020-08-10 16:49:54 -0300

    add new series in json

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,12 +3,12 @@ import '../Hero/style.scss'
 
 class Hero extends React.Component{
   render(){
-    const {img, name, description} = this.props.series[3]
+    const {img, logo, description} = this.props.series[3]
     return(
       <>
       <div className="hero-wrapper">
-        <div className="hero-background" style={{ backgroundImage: `url(${img})` }}> 
-          <p className="hero-title">{name}</p>
+        <div className="hero-background" style={{ backgroundImage:  `url(${img})` }}>
+          <img className="hero-title" src={logo}/>
           <p className="hero-description">{description}</p>    
         </div>
       </div>
